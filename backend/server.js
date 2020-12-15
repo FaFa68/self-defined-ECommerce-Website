b87199e671fadb936f98ea95468e1c0ed92df548
FaFa <farshad.faradji@gmail.com>
2020-12-15 20:10:43 +0100

    Install axios. Load data from backend using Hook.

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ app.get('/api/products', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Server is Ready');
 });
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('Serve at http://localhost:5000');
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
 });
 
