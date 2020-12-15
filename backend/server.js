b8521f435c73c20bd7c67fd39ca9698a721e3ba7
FaFa <farshad.faradji@gmail.com>
2020-12-15 18:48:22 +0100

    Install Express Add backend folder and server.js Move dato to backend folder.

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import data from './data.js';
+
+
+const app = express();
+
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products)
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is Ready');
+});
+
+app.listen(5000, () => {
+    console.log('Serve at http://localhost:5000');
+});
+
